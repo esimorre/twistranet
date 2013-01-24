a6fc7fb16825b480e36a3395b854206dcf3e7b91
macadames <jeanmat.grimaldi@gmail.com>
2013-01-24 16:42:53 +0100

    django 1.4 compliance in progress : - good versions of products - fix command.execute() in bootstrap commands - always use call_command instead

--- twistranet/twistapp/management/commands/twistranet_bootstrap.py
+++ twistranet/twistapp/management/commands/twistranet_bootstrap.py
@@ -3,14 +3,13 @@ Perform database repairings.
 Handy for bootstraping the application!
 """
 from django.core.management.base import BaseCommand, CommandError
-from django.core.management.commands import syncdb
+from django.core.management import call_command
 
 class Command(BaseCommand):
     args = ''
     help = 'Bootstrap your TwistraNet installation. Use this to load initial data and start playing with TN.'
 
     def handle(self, *args, **options):
-        cmd = syncdb.Command()
-        cmd.execute()
+        call_command('syncdb', interactive=False)
         from twistranet.core import bootstrap
         bootstrap.bootstrap()
--- twistranet/twistapp/management/commands/twistranet_update.py
+++ twistranet/twistapp/management/commands/twistranet_update.py
@@ -3,14 +3,13 @@ Perform database repairings.
 Handy for bootstraping the application!
 """
 from django.core.management.base import BaseCommand, CommandError
-from django.core.management.commands import syncdb
+from django.core.management import call_command
 
 class Command(BaseCommand):
     args = ''
     help = 'Update your TwistraNet installation. Use this to load theme in your project.'
 
     def handle(self, *args, **options):
-        cmd = syncdb.Command()
-        cmd.execute()
+        call_command('syncdb', interactive=False)
         from twistranet.core import install
         install.install_theme()
