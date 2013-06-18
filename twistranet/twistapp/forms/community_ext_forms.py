1deae5e4cab0490455e4c5b5342b8b13de1a3ef6
unknown <esimorre@PO8916.IDSI0.si.c-s.fr>
2013-06-18 23:15:18 +0200

    refactoring, WIP

--- twistranet/twistapp/forms/community_ext_forms.py
+++ twistranet/twistapp/forms/community_ext_forms.py
@@ -4,10 +4,34 @@ from twistranet.twistapp.forms.widgets import ResourceWidget
 
 from django.forms import widgets
 
+from twistranet.twistapp.models import Community
+from twistranet.twistapp.forms.form_registry import FormRegistryManager as _FormRegistryManager
+
 class CommunityPrivateForm(community_forms.CommunityForm):
     """ 
     """
     def __init__(self, *args, **kw):
         super(CommunityPrivateForm, self).__init__(*args, **kw)
         self.fields['permissions'].choices = self.fields['permissions'].choices[-1:]
+
+class SimpleFormRegistryManager(_FormRegistryManager):
+    def __init__(self):
+        self._registry_ = {}
+
+class FormRegistryManager:
+    _form_registries = {}
+    
+    def register(self, community_slug, form_class, role=None):
+        reg_name = community_slug
+        if role: reg_name = "%s:%s" % (role, reg_name)
         
+        if not self._form_registries.has_key(reg_name):
+            self._form_registries[reg_name] = SimpleFormRegistryManager()
+        self._form_registries[reg_name].register(form_class)
+    
+    def get_form_registry(self, role=None):
+        if role:
+            return self._form_registries["%s:%s" % (role, self.name)]
+        return self._form_registries[self.name]
+
+form_registry = FormRegistryManager()
--- twistranet/twistapp/views/community_ext_views.py
+++ twistranet/twistapp/views/community_ext_views.py
@@ -1,9 +1,6 @@
 from community_views import CommunityView, CommunityCreate
 from twistranet.twistapp.forms import community_ext_forms
 
-from twistranet.twistapp.models import Community
-from twistranet.twistapp.forms.form_registry import FormRegistryManager
-
 class CommunityExtView(CommunityView):
     """ An extended Community view.
     
@@ -11,21 +8,6 @@ class CommunityExtView(CommunityView):
         it also supports form registering limited to a given community and even to a role.
     """
 
-    _form_registries = {}
-    
-    @classmethod
-    def form_register(cls, community_slug, form_class, role=None):
-        reg_name = community_slug
-        if role: reg_name = "%s:%s" % (role, reg_name)
-        
-        if not cls._form_registries.has_key(reg_name):
-            cls._form_registries[reg_name] = FormRegistryManager()
-        cls._form_registries[reg_name].register(form_class)
-    
-    def get_form_registry(self, role=None):
-        if role:
-            return self._form_registries["%s:%s" % (role, self.name)]
-        return self._form_registries[self.name]
 
     @property
     def template(self):
